f65be615860e9d48580eb4eedff4fe4767dfe385
renedis <renedis@hotmail.com>
2024-12-17 23:32:41 +0100

    Update __init__.py

--- custom_components/one2track/__init__.py
+++ custom_components/one2track/__init__.py
@@ -3,7 +3,7 @@ from requests import ConnectTimeout, HTTPError
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers import device_registry as dr
+from datetime import timedelta
 from .client import get_client, One2TrackConfig
 from .common import (
     CONF_USER_NAME,
@@ -12,8 +12,29 @@ from .common import (
     DOMAIN,
     LOGGER
 )
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 PLATFORMS = ["device_tracker", "sensor"]
+
+class GpsCoordinator(DataUpdateCoordinator):
+    """Coordinator to fetch and manage One2Track data."""
+
+    def __init__(self, hass, api_client):
+        """Initialize the coordinator."""
+        super().__init__(
+            hass,
+            LOGGER,
+            name="One2Track Coordinator",
+            update_interval=timedelta(seconds=30),
+        )
+        self.api_client = api_client
+
+    async def _async_update_data(self):
+        """Fetch the latest data from the API."""
+        try:
+            return await self.api_client.update()
+        except Exception as err:
+            raise UpdateFailed(f"Error fetching data: {err}")
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up One2Track Data from a config entry."""
@@ -22,17 +43,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     config = One2TrackConfig(username=entry.data[CONF_USER_NAME], password=entry.data[CONF_PASSWORD], id=entry.data[CONF_ID])
     api = get_client(config)
-    try:
-        account_id = await api.install()
-    except (ConnectTimeout, HTTPError) as ex:
-        LOGGER.error("Could not retrieve details from One2Track API")
-        raise ConfigEntryNotReady from ex
 
-    if account_id != entry.data[CONF_ID]:
-        LOGGER.error(f"Unexpected account id: {account_id}. Expected: {entry.data[CONF_ID]}")
-        raise ConfigEntryNotReady
+    # Wrap the API client with GpsCoordinator
+    coordinator = GpsCoordinator(hass, api)
+    await coordinator.async_refresh()
 
-    hass.data[DOMAIN][entry.entry_id] = {"api_client": api}
+    hass.data[DOMAIN][entry.entry_id] = {"coordinator": coordinator}
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
