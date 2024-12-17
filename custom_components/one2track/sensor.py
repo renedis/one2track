8d368dd3a339a02a2adccb99a8265f2b9790ac9c
renedis <renedis@hotmail.com>
2024-12-17 23:56:43 +0100

    Update sensor.py

--- custom_components/one2track/sensor.py
+++ custom_components/one2track/sensor.py
@@ -2,20 +2,6 @@ from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .common import DOMAIN
 
-class One2TrackSensor(CoordinatorEntity, SensorEntity):
-
-    def __init__(self, coordinator, device, attribute, name, unit=None):
-        super().__init__(coordinator)
-        self._device = device
-        self._attribute = attribute
-        self._attr_name = f"{name}"
-        self._attr_unique_id = f"one2track_{device['uuid']}_{attribute}"
-        self._attr_unit_of_measurement = unit
-
-    @property
-    def state(self):
-        return self._device["last_location"].get(self._attribute)
-
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
     devices = coordinator.data
@@ -45,3 +31,17 @@ async def async_setup_entry(hass, entry, async_add_entities):
             One2TrackSensor(coordinator, device, "port", "Port", None),
         ])
     async_add_entities(sensors)
+
+class One2TrackSensor(CoordinatorEntity, SensorEntity):
+
+    def __init__(self, coordinator, device, attribute, name, unit=None):
+        super().__init__(coordinator)
+        self._device = device
+        self._attribute = attribute
+        self._attr_name = f"{name}"
+        self._attr_unique_id = f"one2track_{device['uuid']}_{attribute}"
+        self._attr_unit_of_measurement = unit
+
+    @property
+    def state(self):
+        return self._device["last_location"].get(self._attribute)
