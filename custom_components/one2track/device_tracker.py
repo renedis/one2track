ba1f48b5026599917674b84ea28c66c9220e7a8f
renedis <renedis@hotmail.com>
2024-12-18 00:11:29 +0100

    Update device_tracker.py

--- custom_components/one2track/device_tracker.py
+++ custom_components/one2track/device_tracker.py
@@ -19,48 +19,48 @@ class One2TrackTracker(CoordinatorEntity, TrackerEntity):
 
     @property
     def latitude(self):
-        return float(self._device["last_location"]["latitude"])
+        return float(self._device["last_location"].get("latitude", 0.0))
 
     @property
     def longitude(self):
-        return float(self._device["last_location"]["longitude"])
+        return float(self._device["last_location"].get("longitude", 0.0))
 
     @property
     def battery_level(self):
-        return self._device["last_location"]["battery_percentage"]
+        return self._device["last_location"].get("battery_percentage")
 
     @property
     def extra_state_attributes(self):
         return {
             "bron": "One2Track",
-            "battery_level": self._device["last_location"]["battery_percentage"],
-            "breedtegraad": self._device["last_location"]["latitude"],
-            "lengtegraad": self._device["last_location"]["longitude"],
-            "gps_nauwkeurigheid": self._device["last_location"]["accuracy"],
-            "serial_number": self._device["serial_number"],
-            "uuid": self._device["uuid"],
-            "name": self._device["name"],
-            "status": self._device["status"],
-            "phone_number": self._device["phone_number"],
-            "tariff_type": self._device["simcard"]["tariff_type"],
-            "balance_cents": self._device["simcard"]["balance_cents"],
-            "last_communication": self._device["last_location"]["last_communication"],
-            "last_location_update": self._device["last_location"]["last_location_update"],
-            "altitude": self._device["last_location"]["altitude"],
-            "location_type": self._device["last_location"]["location_type"],
-            "address": self._device["last_location"]["address"],
-            "signal_strength": self._device["last_location"]["signal_strength"],
-            "satellite_count": self._device["last_location"]["satellite_count"],
-            "host": self._device["last_location"]["host"],
-            "port": self._device["last_location"]["port"],
+            "battery_level": self._device["last_location"].get("battery_percentage"),
+            "breedtegraad": self._device["last_location"].get("latitude"),
+            "lengtegraad": self._device["last_location"].get("longitude"),
+            "gps_nauwkeurigheid": self._device["last_location"].get("accuracy"),
+            "serial_number": self._device.get("serial_number"),
+            "uuid": self._device.get("uuid"),
+            "name": self._device.get("name"),
+            "status": self._device.get("status"),
+            "phone_number": self._device.get("phone_number"),
+            "tariff_type": self._device["simcard"].get("tariff_type"),
+            "balance_cents": self._device["simcard"].get("balance_cents"),
+            "last_communication": self._device["last_location"].get("last_communication"),
+            "last_location_update": self._device["last_location"].get("last_location_update"),
+            "altitude": self._device["last_location"].get("altitude"),
+            "location_type": self._device["last_location"].get("location_type"),
+            "address": self._device["last_location"].get("address"),
+            "signal_strength": self._device["last_location"].get("signal_strength"),
+            "satellite_count": self._device["last_location"].get("satellite_count"),
+            "host": self._device["last_location"].get("host"),
+            "port": self._device["last_location"].get("port"),
         }
 
     @property
     def device_info(self):
         return {
-            "identifiers": {(DOMAIN, self._device["uuid"])},
+            "identifiers": {(DOMAIN, self._device["name"])},
             "name": f"One2Track {self._device['name']}",
             "manufacturer": "One2Track",
-            "model": "GPS Kids Watch",
-            "sw_version": self._device.get("firmware_version", "Unknown"),
+            "model": "GPS Tracker",
+            "sw_version": self._device.get("serial_number", "Unknown"),
         }
