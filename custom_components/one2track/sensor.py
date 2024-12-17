b6b4b9db3693c74a53d8a2e32c4b04c5c1e2f0a7
renedis <renedis@hotmail.com>
2024-12-17 23:14:40 +0100

    Update sensor.py

--- custom_components/one2track/sensor.py
+++ custom_components/one2track/sensor.py
@@ -1,16 +1,17 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from .common import DOMAIN
 
 class One2TrackSensor(CoordinatorEntity, SensorEntity):
     """One2Track Sensor Entity."""
 
-    def __init__(self, coordinator, device, attribute, name, unit):
+    def __init__(self, coordinator, device, attribute, name, unit=None):
         super().__init__(coordinator)
         self._device = device
         self._attribute = attribute
         self._attr_name = f"{name}"
-        self._attr_unit_of_measurement = unit
         self._attr_unique_id = f"one2track_{device['uuid']}_{attribute}"
+        self._attr_unit_of_measurement = unit
 
     @property
     def state(self):
