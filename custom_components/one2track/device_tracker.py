b0e76244ab355e6af618f03974095ccf77a97838
renedis <renedis@hotmail.com>
2024-12-17 23:45:53 +0100

    Update device_tracker.py

--- custom_components/one2track/device_tracker.py
+++ custom_components/one2track/device_tracker.py
@@ -6,7 +6,6 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 class One2TrackTracker(CoordinatorEntity, TrackerEntity):
-    """Primary One2Track Tracker Entity."""
 
     def __init__(self, coordinator, device):
         super().__init__(coordinator)
@@ -28,7 +27,6 @@ class One2TrackTracker(CoordinatorEntity, TrackerEntity):
 
     @property
     def extra_state_attributes(self):
-        """Return all key attributes."""
         return {
             "bron": "One2Track",
             "battery_level": self._device["last_location"]["battery_percentage"],
