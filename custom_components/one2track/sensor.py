c2621f409dcc7f4b8484ac4ebf19b9a45f04cf09
renedis <renedis@hotmail.com>
2024-12-18 03:27:39 +0100

    Update sensor.py

--- custom_components/one2track/sensor.py
+++ custom_components/one2track/sensor.py
@@ -20,7 +20,7 @@ ICON_MAPPING = {
     "status": "mdi:information",
     "name": "mdi:label",
     "tariff_type": "mdi:sim",
-    "balance_cents": "mdi:currency-usd",
+    "balance_cents": "mdi:currency-eur",
     "host": "mdi:server-network",
     "port": "mdi:ethernet"
 }
@@ -33,24 +33,24 @@ async def async_setup_entry(hass, entry, async_add_entities):
     for device in devices:
         name_prefix = f"{device['name']} watch"
         sensors.extend([
-            One2TrackSensor(coordinator, device, "battery_percentage", f"{name_prefix} Battery Level", "%"),
+            One2TrackSensor(coordinator, device, "battery_percentage", f"{name_prefix} Battery Level", "%", "battery"),
             One2TrackSensor(coordinator, device, "latitude", f"{name_prefix} Latitude", "°"),
             One2TrackSensor(coordinator, device, "longitude", f"{name_prefix} Longitude", "°"),
             One2TrackSensor(coordinator, device, "accuracy", f"{name_prefix} GPS accuracy", "m", fallback=device.get("accuracy")),
             One2TrackSensor(coordinator, device, "altitude", f"{name_prefix} Altitude", "m"),
-            One2TrackSensor(coordinator, device, "signal_strength", f"{name_prefix} Signal Strength", "dBm"),
-            One2TrackSensor(coordinator, device, "satellite_count", f"{name_prefix} Satellite Count", None),
+            One2TrackSensor(coordinator, device, "signal_strength", f"{name_prefix} Signal strength", "dBm", "signal_strength"),
+            One2TrackSensor(coordinator, device, "satellite_count", f"{name_prefix} Satellite count", None),
             One2TrackSensor(coordinator, device, "address", f"{name_prefix} Address", None),
-            One2TrackSensor(coordinator, device, "location_type", f"{name_prefix} Location Type", None),
-            One2TrackSensor(coordinator, device, "last_communication", f"{name_prefix} Last communication", None),
-            One2TrackSensor(coordinator, device, "last_location_update", f"{name_prefix} Last location update", None),
+            One2TrackSensor(coordinator, device, "location_type", f"{name_prefix} Location type", None),
+            One2TrackSensor(coordinator, device, "last_communication", f"{name_prefix} Last communication", None, "timestamp"),
+            One2TrackSensor(coordinator, device, "last_location_update", f"{name_prefix} Last location update", None, "timestamp"),
             One2TrackSensor(coordinator, device, "phone_number", f"{name_prefix} Phone number", None, fallback=device.get("phone_number")),
             One2TrackSensor(coordinator, device, "serial_number", f"{name_prefix} Serial number", None, fallback=device.get("serial_number")),
             One2TrackSensor(coordinator, device, "uuid", f"{name_prefix} UUID", None, fallback=device.get("uuid")),
             One2TrackSensor(coordinator, device, "status", f"{name_prefix} Status", None, fallback=device.get("status")),
             One2TrackSensor(coordinator, device, "name", f"{name_prefix} Name", None, fallback=device.get("name")),
-            One2TrackSensor(coordinator, device, "tariff_type", f"{name_prefix} Tariff Type", None, fallback=device["simcard"].get("tariff_type")),
-            One2TrackSensor(coordinator, device, "balance_cents", f"{name_prefix} Balance Cents", fallback=device["simcard"].get("balance_cents")),
+            One2TrackSensor(coordinator, device, "tariff_type", f"{name_prefix} SIM type", None, fallback=device["simcard"].get("tariff_type")),
+            One2TrackSensor(coordinator, device, "balance_cents", f"{name_prefix} Balance", fallback=device["simcard"].get("balance_cents")),
             One2TrackSensor(coordinator, device, "host", f"{name_prefix} Host", None),
             One2TrackSensor(coordinator, device, "port", f"{name_prefix} Port", None),
         ])
@@ -70,7 +70,7 @@ class One2TrackSensor(CoordinatorEntity, SensorEntity):
         if attribute == "balance_cents":
             self._attr_device_class = "valuta"
 
-        if attribute in ["uuid", "port", "host", "latitude", "longitude", "accuracy", "tariff_type", "name", "status"]:
+        if attribute in ["uuid", "port", "host", "latitude", "longitude", "accuracy", "tariff_type", "name", "status", "last_communication", "last_location_update"]:
             self._attr_entity_registry_enabled_default = False
 
     @property
