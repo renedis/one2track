a47f607fd3cbcfda11c428825bc23e6823060992
renedis <renedis@hotmail.com>
2024-12-18 09:09:10 +0100

    Version 1.1.3

--- custom_components/one2track/device_tracker.py
+++ custom_components/one2track/device_tracker.py
@@ -7,6 +7,7 @@ LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
+    LOGGER.debug(f"Coordinator data structure: {coordinator.data}")  # Log the structure of coordinator data
     devices = coordinator.data
     async_add_entities([One2TrackTracker(coordinator, device) for device in devices], update_before_add=True)
 
@@ -17,42 +18,56 @@ class One2TrackTracker(CoordinatorEntity, TrackerEntity):
         self._attr_unique_id = f"one2track_tracker_{device['uuid']}"
         self._attr_name = f"One2Track {device['name']}"
 
+    def _get_device_data(self):
+        # Safely retrieve device data
+        if isinstance(self.coordinator.data, list):
+            for device_data in self.coordinator.data:
+                if device_data.get("uuid") == self._device["uuid"]:
+                    return device_data
+        elif isinstance(self.coordinator.data, dict):
+            return self.coordinator.data.get(self._device["uuid"], {})
+        return {}
+
     @property
     def latitude(self):
-        return float(self.coordinator.data[self._device["uuid"]]["last_location"].get("latitude", 0.0))
+        device_data = self._get_device_data()
+        return float(device_data.get("last_location", {}).get("latitude", 0.0))
 
     @property
     def longitude(self):
-        return float(self.coordinator.data[self._device["uuid"]]["last_location"].get("longitude", 0.0))
+        device_data = self._get_device_data()
+        return float(device_data.get("last_location", {}).get("longitude", 0.0))
 
     @property
     def battery_level(self):
-        return self.coordinator.data[self._device["uuid"]]["last_location"].get("battery_percentage")
+        device_data = self._get_device_data()
+        return device_data.get("last_location", {}).get("battery_percentage")
 
     @property
     def extra_state_attributes(self):
-        device_data = self.coordinator.data[self._device["uuid"]]["last_location"]
+        device_data = self._get_device_data()
         return {
-            "battery_level": self._device["last_location"].get("battery_percentage"),
-            "breedtegraad": self._device["last_location"].get("latitude"),
-            "lengtegraad": self._device["last_location"].get("longitude"),
-            "gps_nauwkeurigheid": self._device["last_location"].get("accuracy"),
+            "bron": "One2Track",
+            "battery_level": device_data.get("last_location", {}).get("battery_percentage"),
+            "breedtegraad": device_data.get("last_location", {}).get("latitude"),
+            "lengtegraad": device_data.get("last_location", {}).get("longitude"),
+            "gps_nauwkeurigheid": device_data.get("last_location", {}).get("accuracy"),
             "serial_number": self._device.get("serial_number"),
             "uuid": self._device.get("uuid"),
             "name": self._device.get("name"),
             "status": self._device.get("status"),
             "phone_number": self._device.get("phone_number"),
-            "tariff_type": self._device["simcard"].get("tariff_type"),
-            "balance_cents": self._device["simcard"].get("balance_cents"),
-            "last_communication": self._device["last_location"].get("last_communication"),
-            "last_location_update": self._device["last_location"].get("last_location_update"),
-            "altitude": self._device["last_location"].get("altitude"),
-            "location_type": self._device["last_location"].get("location_type"),
-            "address": self._device["last_location"].get("address"),
-            "signal_strength": self._device["last_location"].get("signal_strength"),
-            "satellite_count": self._device["last_location"].get("satellite_count"),
-            "host": self._device["last_location"].get("host"),
-            "port": self._device["last_location"].get("port"),
+            "tariff_type": self._device.get("simcard", {}).get("tariff_type"),
+            "balance_cents": self._device.get("simcard", {}).get("balance_cents"),
+            "last_communication": device_data.get("last_location", {}).get("last_communication"),
+            "last_location_update": device_data.get("last_location", {}).get("last_location_update"),
+            "altitude": device_data.get("last_location", {}).get("altitude"),
+            "location_type": device_data.get("last_location", {}).get("location_type"),
+            "address": device_data.get("last_location", {}).get("address"),
+            "signal_strength": device_data.get("last_location", {}).get("signal_strength"),
+            "satellite_count": device_data.get("last_location", {}).get("satellite_count"),
+            "host": device_data.get("last_location", {}).get("host"),
+            "port": device_data.get("last_location", {}).get("port"),
         }
 
     @property
